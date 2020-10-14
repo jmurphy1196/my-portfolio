bf73b43c0a9c186d7c442f6afd6f24001ac8bff2
jason murphy <jmurphy.1196@gmail.com>
2020-10-14 16:46:59 -0700

    added pokemon project

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-918bc88c9bf03bc8000f.js"
+    "url": "webpack-runtime-d96190ff30cbd96c81e4.js"
   },
   {
     "url": "framework-88a4fbb1a6e60e476d91.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9a0f43a2ce271bdbcf5412e7425c60d4"
+    "revision": "739543e703c337e5cb28bac23d7e611a"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1fdbb67cff7df61010b5b36ed90f9bab"
+    "revision": "4863ab633c48273a6d60a22525d33362"
   },
   {
     "url": "manifest.json",
